4048fbace55a1928fc38a075632fc740627fccc2
Miguel Quiñones Alemán <201-33-5978@uaa.edu>
2026-02-28 07:26:19 -0400

    Add About Page section

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import PlantsPage from "./pages/PlantsPage";
 import CartPage from "./pages/CartPage";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
 
   return (
     <Routes>
-
+      <Route path="/about" element={<AboutPage />} /> 
+      
       <Route path="/" element={<LandingPage />} />
 
       <Route path="/plants" element={<PlantsPage />} />
